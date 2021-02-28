57ca31ceb9eac37f6bf3be213f676fc78ab8b64d
sherstnev6776 <79788927+sherstnev6776@users.noreply.github.com>
2021-03-01 00:36:43 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,27 +1,30 @@
 #simple div
-str_input = input("Delimoe: ")
+str_input = input("a: ")
 
 delimoe = int(str_input)
 #print(type(delimoe))
 
 operation = input("+ / * - ^: ")
 
-str_input2 = input("Delitel: ")
+str_input2 = input("b: ")
 
 delitel = int(str_input2)
 #print(type(delitel))
 
-if operation == '/':
-    result = delimoe / delitel
-elif operation == '+':
-    result = delimoe + delitel
-elif operation == '-':
-    result = delimoe - delitel
-elif operation == '*':
-    result = delimoe * delitel
-elif operation == '^':
-    result = delimoe ** delitel
-else:
+result = None
 
+if operation == '/':
+ result = delimoe / delitel
+elif operation == '+':
+ result = delimoe + delitel
+elif operation == '-':
+ result = delimoe - delitel
+elif operation == '*':
+ result = delimoe * delitel
+elif operation == '^':
+ result = delimoe ** delitel
+else:
+ result = "unknown"
 #print(type(result))
+
 print("Result: " + str(result))
