78180b4ee4e847340cdc954b244a5ea4ee6c6be0
sherstnev6776 <79788927+sherstnev6776@users.noreply.github.com>
2021-03-13 09:12:18 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,42 +1,42 @@
 #simple div
 
-str_command = input("Please type command a + b or a - b: ")
+str_command = input("Please type command a + b or a-b: ")
 
-signFirst = ''
+sign_A = ''
 str_A = ''
 
-signSecond = ''
+sign_B = ''
 str_B = ''
 
 operation = ''
-i=0
+i = 0
+
 while i < len(str_command):
-      print (str_command [i])
-      if str_command [i] == '+' or str_command [i] == '-' or str_command [i] == '*' or str_command [i] == '/' or str_command [i] == '^':
-             if str_A == '':
-                    signFirst = str_command [i]
-             elif operation != '':
-                    signSecond = str_command [i]
-    else:
-        if operation == '':
-            str_A = str_A + str_command [i]
-        else:
-            str_B = str_B + str_command [i]
-    i += 1 #i=i+1
-    pass
-    
-str_A = signFirst + str_A.strip()
-str_B = signSecond + str_B.strip()
+	if str_command[i] == '+' or str_command[i] == '-' or str_command[i] == '*' or str_command[i] == '/' or str_command[i] == '^':
+		if str_A == '':
+			sign_A = str_command[i]
+		elif operation != '':
+			sign_B = str_command[i]
+		else:
+			operation = str_command[i]
+	else:	
+		if operation == '':
+			str_A += str_command[i]
+		else:
+			str_B += str_command[i]
+	i += 1
+
+str_A = sign_A + str_A.strip()
+str_B = sign_B + str_B.strip()
 print(str_A)
 print(str_B)
-    
-    
+
 #str_input = input("A: ")
 
 delimoe = float(str_A)
 #print(type(delimoe))
 
-#operation = input("+ / * - ^ : ")
+#operation = input ("+ / * - ^ : ") 
 
 #str_input2 = input("B: ")
 delitel = float(str_B)
@@ -45,22 +45,21 @@ result = None
 
 if operation == '/':
     if delitel == 0:
-        result = 'inf'
+    	result = 'Inf'
     else:
-        result = delimoe / delitel
+    	result = delimoe / delitel
 #print(type(result))
 elif operation == '+':
-    result = delimoe + delitel
+	result= delimoe + delitel
 elif operation == '-':
-    result = delimoe - delitel
+	result = delimoe - delitel
 elif operation == '*':
-    result = delimoe * delitel
+	result = delimoe * delitel
 elif operation == '^':
-    result = delimoe ** delitel
-    
-else:
-    result = "unknown"
-    
+	result = delimoe ** delitel
+
+else: 
+	result = "unknown"
 #print(type(result))
 
 print("Result: " + str(result))
