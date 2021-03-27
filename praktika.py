77d71d955afc75285416d2f8ecb145fbda15d7cb
sherstnev6776 <79788927+sherstnev6776@users.noreply.github.com>
2021-03-27 11:39:32 +0300

    Update praktika.py

--- praktika.py
+++ praktika.py
@@ -15,14 +15,13 @@ instr = instr.replace(' ', '')
 на выход ?структура данных? с операциями +- и значениями
 (+*-)
 (-2 3.5 2 3 2)
-[{'opr' :  , 'val': -2}, {}, ...]
+[{'opr: , 'val': -2}, {}, ...]
 """
 
 hp_ops = tuple('^')
-mp_ops = tuple('*/')
+mp_ops = ('*','/')
 lp_ops = tuple('+-')
 supported_ops = hp_ops + mp_ops + lp_ops
-#print (supported_ops)
 digital_chars = tuple ('0123456789.-')
 
 actions = list ()
@@ -30,25 +29,19 @@ d = dict()
 d['opr']='First'
 d['val']= ''
 actions.append(d)
-actions.append(d)
-
-result = None
+print(actions)
 
 #премся по строчке, карент -- текущее, делим на операции и числа
 #пока не понимаю, какой алгоритм с отрицательными числами
-cur = ''
+
 i=0
 for i, letter in enumerate(instr): 
-    if letter in supported_ops and (i>0) and cur!='': 
-        actions[-1]['val'] = float (cur)
-        cur = ''
+    if letter in supported_ops: 
         actions.append ({'opr': letter, 'val': ''})
     
     elif letter in digital_chars:
-        cur+=letter
         actions [-1]['val'] += letter
  
-
 print (actions)
 
 
@@ -63,13 +56,17 @@ print (actions)
 
 i = 0
 while i < len(actions):
-	do = actions[i]
-	operation = do.get('opr')
+	action = actions[i]
+	operation = action.get('opr')
 	if operation == hp_ops[0]:
-		actions[i+1]['val'] = actions[i+1].get('val') ** do.get('val')
-		actions.remove(do)
+	    if operation == '^':
+		pre_res = float(actions[i-1].get('val')) ** float(action.get('val'))
+		actions[i+1]['val'] = str(pre_res)
+		del renove[i]
 	else:
 		i += 1
+actions.reverse()
+	
    # pass
 
 
@@ -81,11 +78,31 @@ while i < len(actions):
 -2+3.5*2-9
 2+7-9
 """
-
-  
+i = 0
+result = None
+while i < len(actions):
+	action = actions[i]
+	operation = action.get('opr')
+	if operation in np_ops:
+	    if float(action.get('val')) == 0 and operation == '/':
+	     pass
+	else:
+		i += 1
+actions.reverse()
+print(actions)
 
 #вычислить операции 3го приоритета (сложение и вычитание)
 # -2+7-9 = -4
+
+for action in actions:
+    if type(result)
+        break
+    var_A = result
+    var_B = action.get('opr')
+    if operation in lp_opr:
+        result = eval(val_A + operation + var_B)
+    else:
+        result = var_B
 
 #вывести результат
 print("Result: " + str(result))
